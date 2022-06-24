fb675c72b1d838a18120a0f406265a5db2fe1609
GeoSauer <geoffrey.sauer89@gmail.com>
2022-06-23 21:17:59 -0600

    First Commit

--- app.js
+++ app.js
@@ -1,6 +1,63 @@
 // import needed modules
+import { add } from './utils.js'
+const addInput1 = document.getElementById('add-input-1')
+const addInput2 = document.getElementById('add-input-2')
+const addButton = document.getElementById('add-button')
+const addResult = document.getElementById('add-result')
+addButton.addEventListener('click', () => {
+    console.log(addInput1, addInput2, addButton, addResult)
+    const num1 = Number(addInput1.value);
+    const num2 = Number(addInput2.value);
+    addResult.textContent = add(num1, num2);
+});
 
-// define and grab DOM elements
+import { subtract } from './utils.js'
+const subtractInput1 = document.getElementById('subtract-input-1')
+const subtractInput2 = document.getElementById('subtract-input-2')
+const subtractButton = document.getElementById('subtract-button')
+const subtractResult = document.getElementById('subtract-result')
+subtractButton.addEventListener('click', () => {
+    console.log(subtractInput1, subtractInput2, subtractButton, subtractResult)
+    const num1 = Number(subtractInput1.value);
+    const num2 = Number(subtractInput2.value);
+    subtractResult.textContent = subtract(num1, num2);
+});
+
+import { multiply } from './utils.js'
+const multiplyInput1 = document.getElementById('multiply-input-1')
+const multiplyInput2 = document.getElementById('multiply-input-2')
+const multiplyButton = document.getElementById('multiply-button')
+const multiplyResult = document.getElementById('multiply-result')
+multiplyButton.addEventListener('click', () => {
+    console.log(multiplyInput1, multiplyInput2, multiplyButton, multiplyResult)
+    const num1 = Number(multiplyInput1.value);
+    const num2 = Number(multiplyInput2.value);
+    multiplyResult.textContent = multiply(num1, num2);
+});
+
+import { divide } from './utils.js'
+const divideInput1 = document.getElementById('divide-input-1')
+const divideInput2 = document.getElementById('divide-input-2')
+const divideButton = document.getElementById('divide-button')
+const divideResult = document.getElementById('divide-result')
+divideButton.addEventListener('click', () => {
+    console.log(divideInput1, divideInput2, divideButton, divideResult)
+    const num1 = Number(divideInput1.value);
+    const num2 = Number(divideInput2.value);
+    divideResult.textContent = divide(num1, num2);
+});
+
+import { area } from './utils.js'
+const areaInput1 = document.getElementById('area-input-1')
+const areaInput2 = document.getElementById('area-input-2')
+const areaButton = document.getElementById('area-button')
+const areaResult = document.getElementById('area-result')
+areaButton.addEventListener('click', () => {
+    console.log(areaInput1, areaInput2, areaButton, areaResult)
+    const num1 = Number(areaInput1.value);
+    const num2 = Number(areaInput2.value);
+    areaResult.textContent = area(num1, num2);
+});
 
 // event handlers - what needs to happen?
     // logic and calculations
--- test/tests.js
+++ test/tests.js
@@ -1,18 +1,57 @@
 // IMPORT code to test here
-// import { add } from '../utils.js';
-
+import { add } from '../utils.js';
+import { subtract } from '../utils.js';
+import { multiply } from '../utils.js';
+import { divide } from '../utils.js';
+import { area } from '..utils.js';
 const test = QUnit.test;
 
-test('test your pure functions...', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
+test('add two numbers', (expect) => {
+    
+    const num1 = 3
+    const num2 = 4
+    const expected = 7;   
+    const actual = add(num1, num2);
 
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+    expect.equal(actual, expected);
+});
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
+test('subtract two numbers', (expect) => {
+    
+    const num1 = 7
+    const num2 = 4
+    const expected = 3;   
+    const actual = subtract(num1, num2);
+
+    expect.equal(actual, expected);
+});
+
+test('multiply two numbers', (expect) => {
+    
+    const num1 = 3
+    const num2 = 5
+    const expected = 15;   
+    const actual = multiply(num1, num2);
+
+    expect.equal(actual, expected);
+});
+
+test('divide two numbers', (expect) => {
+    
+    const num1 = 10
+    const num2 = 2
+    const expected = 5;   
+    const actual = divide(num1, num2);
+
+    expect.equal(actual, expected);
+});
+
+test('calculate area', (expect) => {
+    
+    const num1 = 6
+    const num2 = 3
+    const expected = 9;   
+    const actual = multiply(num1, num2);
+
     expect.equal(actual, expected);
 });
